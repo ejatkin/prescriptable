165cbf1022181e9921be9702e864640e6771708b
ejatkin <ejatkin@gmail.com>
2017-01-31 15:02:04 +0000

    Initialised project in Swift

---
+++ LazyDoctor/LazyDoctor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LazyDoctor
+//
+//  Created by Elizabeth Atkin on 31/01/2017.
+//  Copyright © 2017 Elizabeth Atkin, Dan Pelensky, Veronica Sonzini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LazyDoctor/LazyDoctorTests/SystemsOfTheBodyTests.swift
@@ -0,0 +1,25 @@
+//
+//  SystemsOfTheBodyTests.swift
+//  LazyDoctor
+//
+//  Created by Elizabeth Atkin on 31/01/2017.
+//  Copyright © 2017 Elizabeth Atkin, Dan Pelensky, Veronica Sonzini. All rights reserved.
+//
+
+import XCTest
+@testable import LazyDoctor
+
+class SystemsOfTheBodyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+    
+    
+}
