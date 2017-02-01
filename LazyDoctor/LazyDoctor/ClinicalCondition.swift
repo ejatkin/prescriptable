b48eba588250f21b674d989ac81059bd5812da9c
ejatkin <ejatkin@gmail.com>
2017-02-01 11:47:09 +0000

    Created clinical condition class

---
+++ LazyDoctor/LazyDoctor/ClinicalCondition.swift
@@ -0,0 +1,21 @@
+//
+//  ClinicalCondition.swift
+//  LazyDoctor
+//
+//  Created by Elizabeth Atkin on 01/02/2017.
+//  Copyright © 2017 Elizabeth Atkin, Dan Pelensky, Veronica Sonzini. All rights reserved.
+//
+
+import UIKit
+
+class ClinicalCondition: NSObject {
+    
+    
+        let system: String = ""
+        let severity: String = ""
+        let weight: Int = 0
+        let isChild: Bool = false
+        let isPregnant: Bool = false
+        let isPenicillin: Bool = false
+
+}
